16388ade5361b1ed246f52124d826a7e9c464a6f
Your Name <sakshyambanjade@gmail.com>
2025-05-03 00:46:34 +0545

    worked on partial server creation added canvasjs on to the project used api from alphavantage

--- server/galanx/investo/urls.py
+++ server/galanx/investo/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include 
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/', include('stocky.urls')),
-]
+    path('', views.index),
+] + static(settings.STATIC_URL , documents_root = settings.STATIC_ROOT)
